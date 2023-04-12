2d772dc5c76955f6dfc2d932c2fc7d57ddc6ea3a
ICD <iscdr10@gmail.com>
2023-04-12 17:26:27 +0300

    java1

---
+++ CinZodyak/src/CinZodyak.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+public class CinZodyak {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        int year;
+        String burc = "";
+
+        System.out.print("Doğduğunuz yılı giriniz: ");
+        year = inp.nextInt();
+        year = year%12;
+        switch (year) {
+
+            case 0:
+                burc = "Maymun";
+                break;
+            case 1:
+                burc = "Horoz";
+                break;
+            case 2:
+                burc = "Köpek";
+                break;
+            case 3:
+                burc = "Domuz";
+                break;
+            case 4:
+                burc = "Fare";
+                break;
+            case 5:
+                burc = "Öküz";
+                break;
+            case 6:
+                burc = "Kaplan";
+                break;
+            case 7:
+                burc = "Tavşan";
+                break;
+            case 8:
+                burc = "Ejderha";
+                break;
+            case 9:
+                burc = "Yılan";
+                break;
+            case 10:
+                burc = "At";
+                break;
+            case 11:
+                burc = "Koyun";
+                break;
+
+        }System.out.print("Çin Zodyak Burcunuz: " + burc);
+
+    }
+}
